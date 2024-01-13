68d6b6fecb42019448be99f11e637b9783059b0d
Emmanuel Akosa <akosaemmanuel45@gmail.com>
2024-01-13 17:57:25 +0000

    Updates

---
+++ precache-manifest.8041d62c9cd1b09b1980f141687a0731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be66adcdafca6b3ab023ad74d543df0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "07c5642e07248e728f36",
+    "url": "/monsters/static/css/main.aa0fad2c.chunk.css"
+  },
+  {
+    "revision": "26fb5ddf28ba76000eab",
+    "url": "/monsters/static/js/2.f8fb45a4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/monsters/static/js/2.f8fb45a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79070625cbae2febfda4",
+    "url": "/monsters/static/js/3.05fc51e3.chunk.js"
+  },
+  {
+    "revision": "07c5642e07248e728f36",
+    "url": "/monsters/static/js/main.af24f9ad.chunk.js"
+  },
+  {
+    "revision": "3a4761ef4944b8eb78de",
+    "url": "/monsters/static/js/runtime-main.0f84fab2.js"
+  }
+]);
